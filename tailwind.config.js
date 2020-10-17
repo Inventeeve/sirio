9cd7036b546312ef20045dda76bd42c6cfa7c1e8
Luca Corsilli <luca.corsilli@gmail.com>
2020-10-17 13:01:38 +0200

    First commit. Initial strucutre
    Add:
    - Tailwind CSS
    - Tailwind UI
    - PostCSS

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+	theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {},
+  plugins: [
+    require('@tailwindcss/ui'),
+  ]
+}
